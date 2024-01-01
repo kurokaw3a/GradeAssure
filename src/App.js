f65c5c0a6db6a010796e167c12ce7d61acbde67a
Timur <kamaldinov321@gmail.com>
2024-01-01 20:59:57 +0600

    ui-header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,4 @@
-import Header from './components/UI/Header/Header'
-
 const App = () => {
-  return (
-    <div>
-      <Header />
-      <Header variant='menu' />
-      <Header variant='search' blocked />
-      <Header
-        variant='search'
-        selectVariant='teacher'
-        selected
-        selectedCount={5}
-      />
-      <Header variant='search' selected selectedCount={5} />
-    </div>
-  )
+  return <div />
 }
 export default App
